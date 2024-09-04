a0de4a77ca8b014a0efa321b0fb2c21db695bb7e
kashiftariq1997 <kashiftariq848@gmail.com>
2024-09-04 15:39:07 +0500

    Added the functionality of fetching issues

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -17,11 +17,37 @@ class GithubController < ApplicationController
           repo.description = repo_data['description']
           repo.url = repo_data['url']
         end
+
+        # Fetch and save issues for each repository
+        issues_response = client.execute_query(
+          GithubQueries::ISSUES_QUERY,
+          repositoryName: repo.name,
+          owner: github_current_user.username
+        )
+
+        if issues_response && issues_response['data']
+          issues = issues_response['data']['repository']['issues']['nodes']
+          issues.each do |issue_data|
+            repo.github_issues.find_or_create_by!(
+              number: issue_data['number']
+            ) do |issue|
+              issue.title = issue_data['title']
+              issue.body = issue_data['body']
+              issue.state = issue_data['state']
+              issue.html_url = issue_data['url']
+            end
+          end
+        end
       end
 
       redirect_to github_import_page_path, notice: "Repositories and Issues Imported!"
     else
       redirect_to github_import_page_path, alert: "Failed to import repositories."
     end
+  end
+
+  def fetch_issues
+    binding.pry
+    @selected_repo = GithubRepository.find(params[:repository_id])
   end
 end
